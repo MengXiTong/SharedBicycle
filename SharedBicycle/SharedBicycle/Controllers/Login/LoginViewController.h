0eb7238b5ef9714b853bfabe20efd17e3512bcb8
MengXiTong <1317545539@qq.com>
2018-03-05 23:23:24 +0800

    登录界面

---
+++ SharedBicycle/SharedBicycle/Controllers/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  SharedBicycle
+//
+//  Created by 俞健 on 2018/3/5.
+//  Copyright © 2018年 俞健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ SharedBicycle/SharedBicycle/Controllers/Login/LoginViewController.m
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.m
+//  SharedBicycle
+//
+//  Created by 俞健 on 2018/3/5.
+//  Copyright © 2018年 俞健. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self initView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)initView {
+    //点击空白处隐藏键盘
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(keyboardHide:)];
+    //设置成NO表示当前控件响应后会传播到其他控件上，默认为YES。
+    tapGestureRecognizer.cancelsTouchesInView = NO;
+    //将触摸事件添加到当前view
+    [self.view addGestureRecognizer:tapGestureRecognizer];
+}
+
+-(void)keyboardHide:(UITapGestureRecognizer*)tap{
+    //隐藏键盘
+    [[[UIApplication sharedApplication] keyWindow] endEditing:YES];
+}
+
+@end
